313c0e05871f958c28e4a4b123f4aa6cad9d07cd
Kyle Weisse <kyle.weisse@gmail.com>
2021-03-06 13:32:04 -0500

    task2

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import { Title, View, ScrollView } from 'react-native';
+import { Title, View, ScrollView, Text } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
 
 class Contact extends Component {
     render() {
         return (
-            <ScrollView></ScrollView>
+            <ScrollView>
+                <Card>
+                    <Text>
+                        {"1 Nucamp Way\n"}
+                        {"Seattle, WA 98001\n"}
+                        {"U.S.A.\n"}
+                        {"\n"}
+                        {"Phone: 1-206-555-1234\n"}
+                        {"Email: campsites@nucamp.co"}
+                    </Text>
+                </Card>
+            </ScrollView>
         );
     }
 
