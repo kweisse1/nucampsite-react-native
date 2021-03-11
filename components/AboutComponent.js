8efe1cb224443d650548b90143003046ed96057e
Kyle Weisse <kyle.weisse@gmail.com>
2021-03-11 15:08:44 -0500

    bork

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
-import { Title, View, ScrollView, Text, FlatList } from 'react-native';
+import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 class About extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        }
+    static navigationOptions = {
+        title: 'About Us'
     }
+
     render() {
         const renderPartner = ({ item }) => {
             return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png') }}
+                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
                 />
             );
         }
@@ -27,7 +32,7 @@ class About extends Component {
                 <Mission />
                 <Card title="Community Partners">
                     <FlatList
-                        data={this.state.partners}
+                        data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
                     />
@@ -36,9 +41,6 @@ class About extends Component {
         );
     }
 
-    static navigationOptions = {
-        title: 'About Us'
-    }
 
 }
 
@@ -52,4 +54,4 @@ function Mission() {
     )
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
