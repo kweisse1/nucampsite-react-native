630e9c8e21b18d568e9b6a5d1f755f50c782b7f3
Kyle Weisse <kyle.weisse@gmail.com>
2021-03-11 21:29:34 -0500

    errMess isLoading

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -16,6 +16,16 @@ const mapStateToProps = state => {
 function RenderItem(props) {
     const {item} = props;
 
+    if (props.isLoading) {
+        return <Loading/>;
+    }
+    if (props.errMess) {
+        return (
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    }
     if (item) {
         return (
             <Card featuredTitle={item.name} image={{uri: baseUrl+item.image}}>
@@ -41,12 +51,18 @@ class Home extends Component {
             <ScrollView>
                 <RenderItem
                     item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+                    isLoading={this.props.campsites.isLoading}
+                    errMess={this.props.campsites.errMess}
                 />
                 <RenderItem
                     item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
+                    isLoading={this.props.promotions.isLoading}
+                    errMess={this.props.promotions.errMess}
                 />
                 <RenderItem
                     item={this.props.partners.partners.filter(partner => partner.featured)[0]}
+                    isLoading={this.props.partners.isLoading}
+                    errMess={this.props.partners.errMess}
                 />
             </ScrollView>
         );
